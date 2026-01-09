6bb4fa85f8c70b8059ae9bdc486e875ed17c5f2c
fnxL <48607132+fnxL@users.noreply.github.com>
2026-01-09 10:07:23 +0530

    feat(df): add new normalzie_columns and denormalize_columns functions
    - Normalize columns to a standard set of column names and returns a mapping to restore the original column names
    - Denormalize columns back to their original names

--- src/rpatoolkit/df/__init__.py
+++ src/rpatoolkit/df/__init__.py
@@ -1,7 +1,7 @@
+from .normalize_columns import denormalize_columns, normalize_columns
 from .read_excel import read_excel
 from .utils import (
+    find_header_row,
     get_missing_columns,
     reorder_columns,
-    find_header_row,
-    normalize_columns,
 )
