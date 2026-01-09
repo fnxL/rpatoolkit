949539afe947b1c98d1ac5718825342977183fb9
fnxL <48607132+fnxL@users.noreply.github.com>
2026-01-09 17:22:16 +0530

    feat(xl): add apply_borders and convert_to_html methods

---
+++ src/rpatoolkit/xl/convert_to_html.py
@@ -0,0 +1,17 @@
+from xlsx2html import xlsx2html
+from typing import Any
+import re
+
+
+def convert_to_html(source: Any):
+    out_stream = xlsx2html(source)
+    out_stream.seek(0)
+    html_content = out_stream.read()
+
+    # Extract only the table part of the HTML
+    pattern = r"(<table.*?</table>)"
+    match = re.search(pattern, html_content, re.DOTALL | re.MULTILINE)
+    if match:
+        return match.group(1) + "<br><br>"
+
+    return None
